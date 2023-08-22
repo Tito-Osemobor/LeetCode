ec292a72c14544bf38d105254e13216c7aa3d4e6
Tito <91590992+Tito-Osemobor@users.noreply.github.com>
2023-08-21 22:54:47 -0400

    Time: 8 ms (77.3%), Space: 43.8 MB (49.89%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
@@ -5,9 +5,6 @@ class Solution {
         for (int i = 0; i < nums.length; i ++) {
             count = 0;
             for (int j = 0; j < nums.length; j ++) {
-                if (i == j) {
-                    continue;
-                }
                 if (nums[i] > nums[j]) {
                     count ++;
                 }
