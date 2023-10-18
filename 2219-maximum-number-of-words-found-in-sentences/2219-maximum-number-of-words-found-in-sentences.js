e08643ba98f95ee36e8c8c4a3458cd29498ca535
Tito <91590992+Tito-Osemobor@users.noreply.github.com>
2023-10-18 09:37:48 -0400

    Time: 68 ms (6.02%), Space: 44.1 MB (51.99%) - LeetHub

--- 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.js
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.js
@@ -3,17 +3,12 @@
  * @return {number}
  */
 var mostWordsFound = function(sentences) {
-    let count = 0;
-    for (let i = 0; i < sentences.length; i ++) {
-    let currentCount = 1;
-        for (let j = 0; j < sentences[i].length; j ++) {
-            if (sentences[i][j] === " ") {
-                currentCount ++;
-            }
-        }
-        if (currentCount > count) {
-            count = currentCount;
+    let newSentences = sentences.map(sentence => sentence.split(" "));
+    let maxLength = 0;
+    for (let i = 0; i < newSentences.length; i ++) {
+        if (newSentences[i].length > maxLength) {
+            maxLength = newSentences[i].length;
         }
     }
-    return count;
+    return maxLength;
 };
