754560523850cb38a7f79b66f2169bf09a8fcd64
Tito <91590992+Tito-Osemobor@users.noreply.github.com>
2023-10-19 13:38:16 -0400

    Time: 59 ms (8.16%), Space: 41.4 MB (84.18%) - LeetHub

--- 1205-defanging-an-ip-address/1205-defanging-an-ip-address.js
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.js
@@ -3,14 +3,5 @@
  * @return {string}
  */
 var defangIPaddr = function(address) {
-    let result = '';
-    for (let i = 0; i < address.length; i ++) {
-        if (address[i] === '.') {
-            result += '[.]';
-        }
-        else {
-            result += address[i];
-        }
-    }
-    return result;
+    return address.replaceAll('.', '[.]')
 };
