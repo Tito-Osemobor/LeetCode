721e02a39374b2923c621bc86400216073980473
Tito <91590992+Tito-Osemobor@users.noreply.github.com>
2024-03-18 17:53:57 -0400

    Time: 146 ms (24.67%) | Memory: 17 MB (84.56%) - LeetSync

--- 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.py
+++ 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.py
@@ -1,7 +1,7 @@
 class Solution:
     def countWords(self, words1: List[str], words2: List[str]) -> int:
         ans = 0
-        for word in filter(lambda x: words1.count(x) == 1, words1):
-            if words2.count(word) == 1:
+        for word in words1:
+            if words1.count(word) == 1 and words2.count(word) == 1:
                 ans += 1
-        return ans
+        return ans 
