143b9acb521b2f2b1e622bf33115dcfb1225a40a
Tito <91590992+Tito-Osemobor@users.noreply.github.com>
2024-03-22 01:09:47 -0400

    Time: 54 ms (49.90%) | Memory: 16.5 MB (55.83%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.py
+++ 3226-minimum-number-game/minimum-number-game.py
@@ -1,8 +1,6 @@
 class Solution:
     def numberGame(self, nums: List[int]) -> List[int]:
-        nums = sorted(nums)
+        nums.sort()
         for i in range(0, len(nums), 2):
-            alice = nums[i]
-            nums[i] = nums[i+1]
-            nums[i+1] = alice
+            nums[i],nums[i+1]=nums[i+1],nums[i]
         return nums
