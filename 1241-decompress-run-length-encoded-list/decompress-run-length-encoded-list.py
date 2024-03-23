692b96d83e1fe09d79285c483abd39f5dce10791
Tito <91590992+Tito-Osemobor@users.noreply.github.com>
2024-03-23 11:18:21 -0400

    Time: 55 ms (53.31%) | Memory: 17.1 MB (70.33%) - LeetSync

--- 1241-decompress-run-length-encoded-list/decompress-run-length-encoded-list.py
+++ 1241-decompress-run-length-encoded-list/decompress-run-length-encoded-list.py
@@ -2,5 +2,5 @@ class Solution:
     def decompressRLElist(self, nums: List[int]) -> List[int]:
         ans = []
         for i in range(0, len(nums), 2):
-            ans += ([nums[i+1]] * nums[i])
+            ans += [nums[i+1]] * nums[i]
         return ans
