54730e5f4bb3da8ef0b053c3b247ca3c553d06e2
Tito <91590992+Tito-Osemobor@users.noreply.github.com>
2024-10-14 20:29:21 -0400

    Time: 27 ms (97.09%) | Memory: 16.6 MB (25.75%) - LeetSync

--- Level-Easy/389-find-the-difference/find-the-difference.py
+++ Level-Easy/389-find-the-difference/find-the-difference.py
@@ -1,5 +1,7 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        ss = sum(ord(i) for i in s)
-        ts = sum(ord(i) for i in t)
-        return chr(ts - ss)
+        sum_t = sum(map(ord, t))
+        sum_s = sum(map(ord, s))
+        additional_char = chr(sum_t - sum_s)
+        
+        return additional_char
