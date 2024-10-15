db026324ede21c1c50278583a49be10daa847340
Tito <91590992+Tito-Osemobor@users.noreply.github.com>
2024-10-14 20:25:25 -0400

    Time: 39 ms (48.01%) | Memory: 16.5 MB (67.43%) - LeetSync

--- Level-Easy/389-find-the-difference/find-the-difference.py
+++ Level-Easy/389-find-the-difference/find-the-difference.py
@@ -1,10 +1,6 @@
-from collections import Counter
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        sCounter = Counter(s)
-        tCounter = Counter(t)
-
-        sCounter.subtract(tCounter)
-
-        difference = "".join(char for char, count in sCounter.items() if count < 0)
-        return difference
+        result = 0
+        for char in s + t:
+            result ^= ord(char)
+        return chr(result)
