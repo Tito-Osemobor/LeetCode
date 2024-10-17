136b170064f14e72c772e6b3b88b6014312e8f63
Tito <91590992+Tito-Osemobor@users.noreply.github.com>
2024-10-16 21:02:56 -0400

    Time: 45 ms (40.89%) | Memory: 17.8 MB (15.13%) - LeetSync

--- Level-Easy/412-fizz-buzz/fizz-buzz.py
+++ Level-Easy/412-fizz-buzz/fizz-buzz.py
@@ -1,12 +1,15 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         answer = []
-        for i in range(1, n+1):
-            if i % 3 == 0 and i % 5 == 0:
+        for i in range(1, n + 1):
+            divisible_by_3 = (i % 3 == 0)
+            divisible_by_5 = (i % 5 == 0)
+            
+            if divisible_by_3 and divisible_by_5:
                 answer.append("FizzBuzz")
-            elif i % 3 == 0:
+            elif divisible_by_3:
                 answer.append("Fizz")
-            elif i % 5 == 0:
+            elif divisible_by_5:
                 answer.append("Buzz")
             else:
                 answer.append(str(i))
