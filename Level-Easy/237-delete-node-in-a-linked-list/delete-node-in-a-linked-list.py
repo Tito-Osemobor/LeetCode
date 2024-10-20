bfa8ab7b32cfca93f8e1957e405b5c3df3727372
Tito <91590992+Tito-Osemobor@users.noreply.github.com>
2024-10-19 22:31:26 -0400

    Time: 36 ms (72.83%) | Memory: 17 MB (26.69%) - LeetSync

--- Level-Easy/237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.py
+++ Level-Easy/237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.py
@@ -10,5 +10,8 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
+        if node is None or node.next is None:
+            return
+
         node.val = node.next.val
         node.next = node.next.next
