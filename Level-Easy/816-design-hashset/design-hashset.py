e33b650dddd29166923ebf7b316689a1d5b74ed6
Tito <91590992+Tito-Osemobor@users.noreply.github.com>
2024-10-20 21:04:06 -0400

    Time: 22 ms (99.41%) | Memory: 22 MB (45.68%) - LeetSync

--- Level-Easy/816-design-hashset/design-hashset.py
+++ Level-Easy/816-design-hashset/design-hashset.py
@@ -1,14 +1,12 @@
 class MyHashSet:
     def __init__(self):
-        self.set = []
+        self.set = set()
     
     def add(self, key: int) -> None:
-        if key not in self.set:
-            self.set.append(key)
+        self.set.add(key)
     
     def remove(self, key: int) -> None:
-        if key in self.set:
-            self.set.remove(key)
+        self.set.discard(key)
     
     def contains(self, key: int) -> bool:
         return key in self.set
