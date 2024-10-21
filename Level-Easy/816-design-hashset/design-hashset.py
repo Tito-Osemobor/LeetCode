1d2981221f9386544a9ef4f1c587fd1c19c7c207
Tito <91590992+Tito-Osemobor@users.noreply.github.com>
2024-10-20 21:02:47 -0400

    Time: 701 ms (24.80%) | Memory: 21.5 MB (58.13%) - LeetSync

--- Level-Easy/816-design-hashset/design-hashset.py
+++ Level-Easy/816-design-hashset/design-hashset.py
@@ -1,29 +1,17 @@
 class MyHashSet:
     def __init__(self):
-        # Initialize an array of empty lists (buckets), we use 1000 for simplicity
-        self.size = 1000
-        self.data = [[] for _ in range(self.size)]
-    
-    def hash(self, key: int) -> int:
-        # A simple hash function that maps a key to an index
-        return key % self.size
+        self.set = []
     
     def add(self, key: int) -> None:
-        # Add the key to the appropriate bucket, if not already present
-        index = self.hash(key)
-        if key not in self.data[index]:
-            self.data[index].append(key)
+        if key not in self.set:
+            self.set.append(key)
     
     def remove(self, key: int) -> None:
-        # Remove the key from the bucket, if it exists
-        index = self.hash(key)
-        if key in self.data[index]:
-            self.data[index].remove(key)
+        if key in self.set:
+            self.set.remove(key)
     
     def contains(self, key: int) -> bool:
-        # Check if the key is in the appropriate bucket
-        index = self.hash(key)
-        return key in self.data[index]
+        return key in self.set
         
 
 
