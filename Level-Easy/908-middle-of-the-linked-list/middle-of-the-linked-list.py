572227b16868ea55e5c48a224ad073554f133a67
Tito <91590992+Tito-Osemobor@users.noreply.github.com>
2024-10-21 21:18:33 -0400

    Time: 0 ms (100.00%) | Memory: 16.7 MB (5.51%) - LeetSync

--- Level-Easy/908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ Level-Easy/908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -5,9 +5,8 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        templ = []
-        while head != None:
-            templ.append(head)
-            head = head.next
-        length = len(templ) 
-        return templ[length // 2]
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
