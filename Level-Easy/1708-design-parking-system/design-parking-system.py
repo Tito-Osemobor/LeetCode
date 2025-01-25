c78a85ff553bd5e7d8dcd21c218e669be51668a8
Tito <91590992+Tito-Osemobor@users.noreply.github.com>
2025-01-25 00:03:45 -0500

    Time: 7 ms (90.93%) | Memory: 18.3 MB (28.87%) - LeetSync

--- Level-Easy/1708-design-parking-system/design-parking-system.py
+++ Level-Easy/1708-design-parking-system/design-parking-system.py
@@ -1,18 +1,15 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        self.slots = {
-            "1": big,
-            "2": medium,
-            "3": small
-        }
+        self.cnt = [0, big, medium, small]
 
     def addCar(self, carType: int) -> bool:
-        if self.slots[str(carType)] > 0:
-            self.slots[str(carType)] -= 1
-            return True
-        else:
+        if self.cnt[carType] == 0:
             return False
+        self.cnt[carType] -= 1
+        return True
+        
+
 
 # Your ParkingSystem object will be instantiated and called as such:
 # obj = ParkingSystem(big, medium, small)
