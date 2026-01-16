8267e17ac8fd7bcde31c2c245210ceb63840085e
Tito <91590992+Tito-Osemobor@users.noreply.github.com>
2026-01-15 21:58:50 -0500

    Time: 11 ms (83.11%) | Memory: 22.1 MB (30.89%) - LeetSync

--- Level-Medium/49-group-anagrams/group-anagrams.py
+++ Level-Medium/49-group-anagrams/group-anagrams.py
@@ -1,22 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        listOfLists = []
-        dictionaryOfDictionaries = {} # maps index of listOfLists to character count dict
-        for s in strs:
-            found = False
-            # Create character count for current string
-            current_count = {}
-            for char in s:
-                current_count[char] = current_count.get(char, 0) + 1
-            
-            # Check against existing groups
-            for i in range(len(listOfLists)):
-                if current_count == dictionaryOfDictionaries[i]:
-                    listOfLists[i].append(s)
-                    found = True
-                    break
-            
-            if not found:
-                dictionaryOfDictionaries[len(listOfLists)] = current_count
-                listOfLists.append([s])
-        return listOfLists
+        answer = defaultdict(list)
+        for str in strs:
+            sortedS = "".join(sorted(str))
+            answer[sortedS].append(str)
+        return list(answer.values())
